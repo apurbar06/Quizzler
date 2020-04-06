81ca74cfc18c97484fb455d6913d6ab924b8476d
Apurba Roy <apurbar06@gmail.com>
2020-04-06 13:21:28 +0530

    finish building

--- app/src/main/java/com/example/quizzler/MainActivity.java
+++ app/src/main/java/com/example/quizzler/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.quizzler;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,14 +15,14 @@ public class MainActivity extends Activity {
 
     private final static String TAG = "MainActivity";
 
-    // TODO: Declare constants here
-
-
     // TODO: Declare member variables here:
     Button mTrueButton;
     Button mFalseButton;
     TextView mQuestionTextView;
+    TextView mScoreTextView;
+    ProgressBar mProgressBar;
     int mIndex;
+    int mScore;
     int mQuestion;
 
 
@@ -40,6 +43,9 @@ public class MainActivity extends Activity {
             new TrueFalse(R.string.question_13,true)
     };
 
+    // TODO: Declare constants here
+    final int PROGRESS_BAR_INCREMENT = (int) Math.ceil(100.0 / mQuestionBank.length);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,7 +54,10 @@ public class MainActivity extends Activity {
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton = (Button) findViewById(R.id.false_button);
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        mScoreTextView = (TextView) findViewById(R.id.score);
+        mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
 
+        //initial question
         mQuestion = mQuestionBank[mIndex].getQuestionID();
         mQuestionTextView.setText(mQuestion);
 
@@ -56,7 +65,8 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
 //                Log.d(TAG, "onClick: true button is clicked.");
-//                Toast.makeText(getApplicationContext(),"True pressed.",Toast.LENGTH_SHORT).show();
+                //first have to check answer then updated the question
+                checkAnswer(true);
                 updateQuestion();
             }
         });
@@ -66,16 +76,56 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
 //                Toast myToast = Toast.makeText(getApplicationContext(),"False pressed !!!",Toast.LENGTH_SHORT);
 //                myToast.show();
+                //first have to check answer then updated the question
+                checkAnswer(false);
                 updateQuestion();
             }
         });
 
-
     }
 
+    /*
+    * Update the view after every click on the button
+    * Increment the progress bar after every press*/
     private void updateQuestion() {
         mIndex = (mIndex + 1) % mQuestionBank.length;
+
+        //helps to close the application after finished all questions
+        if (mIndex == 0) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("Game Over");
+            //makes unable  to disappear the alert box by clicking somewhere else on the screen
+            alert.setCancelable(false);
+            alert.setMessage("You scored " + mScore + " points!");
+            alert.setPositiveButton("Close application", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            alert.show();
+        }
+
         mQuestion = mQuestionBank[mIndex].getQuestionID();
         mQuestionTextView.setText(mQuestion);
+        // progress bar will increment PROGRESS_BAR_INCREMENT portion out of 100
+        mProgressBar.incrementProgressBy(PROGRESS_BAR_INCREMENT);
     }
+
+    /*
+    * Check whether the chosen answer is correct or not
+    * Update the score
+    * Show toast message*/
+    private void checkAnswer(boolean userSelection) {
+        boolean correctAnswer = mQuestionBank[mIndex].isAnswer();
+
+        if(userSelection == correctAnswer) {
+            Toast.makeText(getApplicationContext(),R.string.correct_toast,Toast.LENGTH_SHORT).show();
+            mScore = mScore + 1;
+            mScoreTextView.setText("Score " + mScore + "/" + mQuestionBank.length);
+        } else{
+            Toast.makeText(getApplicationContext(),R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
